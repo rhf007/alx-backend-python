175bd29ab96811570258b1e4b51230e3db820edd
Rahaf Hazem <110139745+rhf007@users.noreply.github.com>
2024-12-14 13:37:25 +0200

    Create 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Complex types
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    sum_list
+    """
+    return sum(input_list)
